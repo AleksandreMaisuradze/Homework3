bbec79c51350bd4c981ae421ea1817781ff0dc56
Aleksandre Maisuradze <aleksandremaisuradze11@gmail.com>
2023-11-19 23:53:52 +0400

    6,8,9

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 1
+console.log("///1");
 function positiveSum() {
     let sum = 0;
     for (let i = 0; i < array.length; i++) {
@@ -14,7 +14,7 @@ let result = positiveSum();
 
 console.log(result);
 
-// 2
+console.log("///2");
 
 function numberSum() {
     let sum = 0;
@@ -32,7 +32,7 @@ console.log(result1);
 
 
 
-// 3
+console.log("///3");
 
 function userProperties() {
     let user = {
@@ -56,20 +56,64 @@ let result2 = userProperties();
 
 console.log(result2);
 
-// 4
 
 
-//5
+console.log("///5");
 
-function evenOrOdd(number){
-    if(number % 2 == 0){
+function evenOrOdd(number) {
+    if (number % 2 == 0) {
         console.log('this number is even');
     }
-    else{
+    else {
         console.log("this number is odd");
     }
 }
 
 evenOrOdd(5)
 
-//6
+console.log("/////6");
+
+let array2 = ['html', 'css', 'python', 'javascript', 'bootstrap'];
+
+for (let i = 0; i < array2.length; i++) {
+    if (array2[i].length > 4) {
+        console.log(array2[i]);
+    }
+}
+
+console.log("///8");
+
+age = () => {
+    let user = {
+        firstname: 'tom',
+        lastname: 'tobias',
+        age: 32,
+        isloggedin: true
+    }
+    if (user.age >= 18) {
+        console.log("სრულწლოვანი");
+    }
+    else {
+        console.log("არასრულწლოვანი");
+    }
+}
+
+age()
+
+console.log("///9");
+
+
+twoNumber = (num1, num2) => {
+    if(num1 > num2){
+        console.log("num1 is largest");
+    }
+    else if(num1 < num2){
+        console.log("num2 is largest");
+    }
+    else {
+        console.log("error");
+    }
+}
+
+
+twoNumber(14, 5)
